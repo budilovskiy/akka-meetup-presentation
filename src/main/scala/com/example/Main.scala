043dc1b2c7eae82d92166be6785063df96c739c2
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2019-12-03 11:58:13 +0200

    2. Creating actor system

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -1,5 +1,14 @@
 package com.example
 
+import akka.actor.ActorSystem
+
+import scala.concurrent.ExecutionContextExecutor
+
 object Main extends App {
-  
+  // Actors needs an ActorSystem
+  val system: ActorSystem = ActorSystem("HelloSystem")
+
+  // Using dispatcher as execution context in whole application
+  // This dispatcher is used for all actors in the actor system by default
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 }
