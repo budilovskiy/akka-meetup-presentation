7102adb8c5689448bf6cf6350e18d1a9bfa69580
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2019-12-07 19:07:04 +0200

    6. Added StartProject message

--- src/main/scala/com/example/ProjectManager.scala
+++ src/main/scala/com/example/ProjectManager.scala
@@ -2,12 +2,12 @@ package com.example
 
 import akka.actor.{Actor, ActorLogging}
 
-case class StartProject(name: String)
+case class StartProject(projectName: String)
 
-class ProjectManager extends Actor with ActorLogging {
+class ProjectManager(name: String) extends Actor with ActorLogging {
   // Actor always starts new project on receiving StartProject message
   def receive: Receive = {
-    case StartProject(name) =>
-      log.info(s"Starting project $name.")
+    case StartProject(projectName) =>
+      log.info(s"Starting project $projectName.")
   }
 }
