500b8d20431db7ffc0938429bbcfa2002284da82
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2019-12-09 20:54:00 +0200

    13. Add TechLead behavior

--- src/main/scala/com/example/ProjectManager.scala
+++ src/main/scala/com/example/ProjectManager.scala
@@ -5,10 +5,19 @@ import akka.actor.{Actor, ActorLogging}
 case class StartProject(projectName: String)
 object Finish
 
+case class Task(id: Int, description: String, done: Boolean = false)
+
 class ProjectManager(name: String) extends Actor with ActorLogging {
   def Idle: Receive = {
     case StartProject(projectName) =>
       log.info(s"Starting project $projectName.")
+
+      val projectTasks = List(
+        Task(1, "Initialize project."),
+        Task(2, "Create backend."),
+        Task(3, "Crate frontend."),
+        Task(4, "Deploy application")
+      )
       context.become(Working(projectName))
   }
 
--- src/main/scala/com/example/TechnicalLeader.scala
+++ src/main/scala/com/example/TechnicalLeader.scala
@@ -2,8 +2,11 @@ package com.example
 
 import akka.actor.{Actor, ActorLogging}
 
-case class Develop(projectName: String, details: String)
+case class Develop(projectName: String, tasks: Seq[Task])
 
 class TechnicalLeader(name: String) extends Actor with ActorLogging {
-  def receive: Receive = Actor.emptyBehavior
+  def receive: Receive = {
+    case Develop(projectName, tasks) =>
+      log.info(s"Starting to develop $projectName.")
+  }
 }
