27276dadfc3b4776b4ee45f4269fef3c1c7cebfb
Roman Pustika <r.pustika@alpha-serve.com>
2019-12-11 11:27:24 +0200

    Extends Developer actor

--- src/main/scala/com/example/Developer.scala
+++ src/main/scala/com/example/Developer.scala
@@ -3,10 +3,13 @@ package com.example
 import akka.actor.{Actor, ActorLogging}
 
 case class WorkOnTask(task: Task)
+case class TaskFinished(task: Task)
 
 class Developer extends Actor with ActorLogging {
   def receive: Receive = {
     case WorkOnTask(task) =>
       log.info(s"Start working on $task")
+      val doneTask = task.copy(done = true)
+      sender() ! TaskFinished(doneTask)
   }
 }
