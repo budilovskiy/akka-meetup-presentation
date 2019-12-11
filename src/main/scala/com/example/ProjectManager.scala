0164862fb4d147c2d3261b94904378aa9d396067
Roman Pustika <r.pustika@alpha-serve.com>
2019-12-11 11:31:32 +0200

    Extends ProjectManager actor

--- src/main/scala/com/example/ProjectManager.scala
+++ src/main/scala/com/example/ProjectManager.scala
@@ -4,10 +4,14 @@ import akka.actor.{Actor, ActorLogging, Props}
 
 case class StartProject(projectName: String)
 object Finish
+object CheckTasks
 
 case class Task(id: Int, description: String, done: Boolean = false, succeeded: Boolean = false)
 
 class ProjectManager(name: String) extends Actor with ActorLogging {
+  private val techLead = context.actorOf(Props(new TechnicalLeader("Bob")), "tech_lead_actor")
+  private val qaLead = context.actorOf(Props(new QaLeader("Jon")), "QA_lead_actor")
+
   def Idle: Receive = {
     case StartProject(projectName) =>
       log.info(s"Starting project $projectName.")
@@ -19,13 +23,30 @@ class ProjectManager(name: String) extends Actor with ActorLogging {
         Task(4, "Deploy application")
       )
 
-      val techLead = context.actorOf(Props(new TechnicalLeader("Bob")), "tech_lead_actor")
       techLead ! Develop(projectName, projectTasks)
 
-      context.become(Working(projectName))
+      context.become(Working(projectName, projectTasks))
   }
 
-  def Working(projectName: String): Receive = {
+  def Working(projectName: String, tasks: Seq[Task]): Receive = {
+
+    case command: TaskForTest =>
+      qaLead ! command
+
+    case TestedTask(task) if task.succeeded =>
+      val newPullTasks = tasks.filterNot(_.id == task.id) :+ task
+      context.become(
+        Working(projectName, newPullTasks)
+      )
+      self ! CheckTasks
+
+    case TestedTask(task) if !task.succeeded =>
+      techLead ! FailedTask(task.copy(done = false))
+
+    case CheckTasks =>
+      if (tasks.forall(t => t.succeeded && t.done))
+        context.self ! Finish
+
     case Finish =>
       log.info(s"Finish current project $projectName.")
       context.become(Idle)
