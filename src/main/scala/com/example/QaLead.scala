82b64c07fda71f66b4768fbd024cc0daafba2b93
Roman Pustika <r.pustika@alpha-serve.com>
2019-12-11 11:25:48 +0200

    Extends QA lead actor

--- src/main/scala/com/example/QaLead.scala
+++ src/main/scala/com/example/QaLead.scala
@@ -1,11 +1,24 @@
 package com.example
 
-import akka.actor.{Actor, ActorLogging}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 
 case class TestedTask(task: Task)
 
 class QaLeader(name: String)  extends Actor with ActorLogging {
 
-  def receive: Receive = Actor.emptyBehavior
+  val testers: Seq[ActorRef] = List(
+    context.actorOf(Props(new Qa()), "QA_1"),
+    context.actorOf(Props(new Qa()), "QA_2")
+  )
+
+  def receive: Receive = {
+
+    case command: TaskForTest =>
+      val randomTester: ActorRef = testers(scala.util.Random.nextInt(testers.length))
+      randomTester ! command
+
+    case command: TestedTask =>
+      context.parent ! command
+  }
 
 }
